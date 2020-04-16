1f644777c19e11459c2bd257659c7cceb101a67a
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-04-16 15:21:24 -0700

    Navigation > created const sections & const navLinks to dynaimcally manage navigation titles >>> used .map()

--- pencil-app/src/Navigation/Navigation.js
+++ pencil-app/src/Navigation/Navigation.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import './Navigation.css'
+import "./Navigation.css";
 
-function Navigation ({LogoTitle}) {
+function Navigation({ LogoTitle }) {
+  const sections = ["Home", "About", "Services", "Products", "Contact"];
+  const navLinks = sections.map(section => {
+    return (
+      <li>
+        <a href={"#" + section}>{section}</a>
+      </li>
+    );
+  });
+
   return (
     <div>
-        <nav>
-        <h2 className='logo'>{LogoTitle}</h2>
-        <ul>
-            <li><a href='#home'>Home</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#services'>Services</a></li>
-            <li><a href='#products'>Products</a></li>
-            <li><a href='#contact'>Contact</a></li>
-        </ul>
-        </nav>
+      <nav>
+        <h2 className="logo">{LogoTitle}</h2>
+        <ul>{navLinks}</ul>
+      </nav>
     </div>
   );
 }
