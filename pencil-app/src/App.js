a9dd86e873927c6a44a869519ea7a0d3d52295f6
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-04-17 11:57:56 -0700

    Services > created, styled and spaced through WWP

--- pencil-app/src/App.js
+++ pencil-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Navigation from './Navigation/Navigation'
 import Header from './Header/Header'
+import Services from './Services/Services'
 
 function App() {
   return (
     <div className="App">
         <Navigation LogoTitle='Pencil Co.'/>
         <Header title='Pencil Co.' button='Graphium'/>
+        <Services />
     </div>
   );
 }
