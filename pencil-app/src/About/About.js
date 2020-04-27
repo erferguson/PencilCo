4c4bd41ec066d3aa0b012e0bb98531ed3f515769
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-04-27 13:52:18 -0700

    About > added heroImage and basic <p>'s x 3

--- pencil-app/src/About/About.js
+++ pencil-app/src/About/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import BackgroundHero from '../img/portfolio-4.jpg'
+import BackgroundHero from '../img/PencilCo..png'
 
 const heroImage = {
     backgroundImage: `url( ${BackgroundHero})`,
-    height: '15vh',
+    height: '30vh',
     backgroundSize: "cover"
 }
 
@@ -11,8 +11,14 @@ function About () {
     return (
         <div>
             <header style={heroImage}>
-                <h2>About</h2>
             </header>
+            <p>The funny thing about being a family-owned, 100-year-old pencil factory is that a pencil is an enduring object in the midst of change — including technology and major shifts to American manufacturing.
+            </p>
+            <p>Through steady management, a dedicated team of employees, loyal customers, and a vibrant manufacturing community in our hometown of Napa, CA, we’re stronger than ever providing manufacturing jobs and an American-made product.
+            </p>
+            <p>A pencil is simple, utilitarian, and yet the starting point for creativity, education, and human connection. We’re proud of our product and our capabilities as a manufacturer, and we are eager to help you create a high-quality, American-made pencil just right for your company or brand.
+            </p>
+
         </div>
     )
 }
